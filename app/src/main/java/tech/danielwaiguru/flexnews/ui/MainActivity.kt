427e58662de337b4883de3be99e72548d0154776
Daniel Waiguru <danielirungu91@gmail.com>
2020-05-26 15:32:41 +0300

    feat: bottom navigation

--- app/src/main/java/tech/danielwaiguru/flexnews/ui/MainActivity.kt
+++ app/src/main/java/tech/danielwaiguru/flexnews/ui/MainActivity.kt
@@ -2,6 +2,9 @@ package tech.danielwaiguru.flexnews.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 import tech.danielwaiguru.flexnews.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
