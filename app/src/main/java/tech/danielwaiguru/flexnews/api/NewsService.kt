11b09506d8cefd07206754357014d297443833ec
Daniel Waiguru <danielirungu91@gmail.com>
2020-05-28 12:55:40 +0300

    added service interface

---
+++ app/src/main/java/tech/danielwaiguru/flexnews/api/NewsService.kt
@@ -0,0 +1,37 @@
+package tech.danielwaiguru.flexnews.api
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+import tech.danielwaiguru.flexnews.data.NewsResponse
+
+/**
+ * Service interface 
+ */
+interface NewsService {
+    companion object {
+        const val API_KEY = "10ccffe776b14ac4829aab687f590fee"
+    }
+    @GET("/v2/top-headlines")
+    suspend fun trendingNews(
+        @Query("country")
+        countryCode: String = "ke",
+        @Query("page")
+        pageNum: Int = 1,
+        @Query("api_key")
+        api_key: String = API_KEY
+    ):Response<NewsResponse>
+
+    /**
+     * News search
+     */
+    @GET("/v2/everything")
+    suspend fun searchNews(
+        @Query("q")
+        queryEveryThing :String,
+        @Query("page")
+        pageNum: Int = 1,
+        @Query("api_key")
+        api_key: String = API_KEY
+    ): Response<NewsResponse>
+}
