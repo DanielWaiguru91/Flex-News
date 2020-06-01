9007eeaef160b9e4bb25331ef8085e0009a7b559
Daniel Waiguru <danielirungu91@gmail.com>
2020-06-01 11:03:40 +0300

    added viewmodel class

---
+++ app/src/main/java/tech/danielwaiguru/flexnews/ui/NewsViewModel.kt
@@ -0,0 +1,8 @@
+package tech.danielwaiguru.flexnews.ui
+
+import androidx.lifecycle.ViewModel
+import tech.danielwaiguru.flexnews.data.NewsRepository
+
+class NewsViewModel(newsRepository: NewsRepository) : ViewModel(){
+
+}
