6f67bf711512f7a6d8626440f5caf4d763c1b862
Daniel Waiguru <danielirungu91@gmail.com>
2020-07-12 01:52:24 +0300

    Added network error alert dialog

--- app/src/main/java/tech/danielwaiguru/flexnews/fragments/TrendingNewsFragment.kt
+++ app/src/main/java/tech/danielwaiguru/flexnews/fragments/TrendingNewsFragment.kt
@@ -5,15 +5,16 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_trending_news.*
 import tech.danielwaiguru.flexnews.R
 import tech.danielwaiguru.flexnews.adapters.NewsAdapter
+import tech.danielwaiguru.flexnews.models.Result
 import tech.danielwaiguru.flexnews.ui.MainActivity
 import tech.danielwaiguru.flexnews.ui.NewsViewModel
-import tech.danielwaiguru.flexnews.models.Result
 
 class TrendingNewsFragment : Fragment(){
     private lateinit var viewModel: NewsViewModel
@@ -41,10 +42,11 @@ class TrendingNewsFragment : Fragment(){
                 }
                 is Result.Loading ->{
                     showProgressBar()
+                    noInternetConnectionDialog()
                 }
                 is Result.Failure ->{
                     hideProgressBar()
-                    Log.e("Trending News Fragment", "Error occurred ")
+                    Log.e("TrendingNewsFragment", "Error occurred ")
                 }
             }
         })
@@ -62,5 +64,24 @@ class TrendingNewsFragment : Fragment(){
             adapter = newsAdapter
             layoutManager = LinearLayoutManager(activity)
         }
+    }
+    private fun noInternetConnectionDialog(){
+        val dialogTitle = getString(R.string.dialogTitle)
+        val dialogMessage = getString(R.string.dialogMessage)
+        val positiveButton = getString(R.string.retryButton)
+        val negativeButton = getString(R.string.cancelButton)
+        activity?.let {
+            val networkDialog = AlertDialog.Builder(it)
+            networkDialog.setTitle(dialogTitle)
+            networkDialog.setMessage(dialogMessage)
+            networkDialog.setIcon(R.drawable.ic_warning)
+            networkDialog.setPositiveButton(positiveButton) { _, _ ->  }
+            networkDialog.setNegativeButton(negativeButton) { dialog, _ ->
+                dialog.dismiss()
+                activity?.finish()
+            }
+            networkDialog.create().show()
+        }
+
     }
 }
