e9921fb9482352a584697ede0e81353d9df4454f
Daniel Waiguru <danielirungu91@gmail.com>
2020-09-26 05:48:26 +0300

    add News Paging Data Source

--- app/src/main/java/tech/danielwaiguru/flexnews/common/Constants.kt
+++ app/src/main/java/tech/danielwaiguru/flexnews/common/Constants.kt
@@ -1,7 +1,6 @@
 package tech.danielwaiguru.flexnews.common
 
 object Constants {
-    const val ARTICLE_URL = "url"
     const val PAGE_INDEX = 1
     const val NETWORK_PAGE_SIZE = 50
     const val DB_NAME = "articles"
