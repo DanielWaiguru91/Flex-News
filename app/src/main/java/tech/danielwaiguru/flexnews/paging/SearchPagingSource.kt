7ed4afc461204d68ed586fdf083258a00597c173
Daniel Waiguru <danielirungu91@gmail.com>
2020-11-09 10:19:23 +0300

    add a NewsPagingSource to fetch all the trending news

--- app/src/main/java/tech/danielwaiguru/flexnews/paging/SearchPagingSource.kt
+++ app/src/main/java/tech/danielwaiguru/flexnews/paging/SearchPagingSource.kt
@@ -7,23 +7,24 @@ import tech.danielwaiguru.flexnews.common.Constants.PAGE_INDEX
 import tech.danielwaiguru.flexnews.models.Article
 import tech.danielwaiguru.flexnews.networking.NewsApiService
 
-class NewsPagingSource(private val apiService: NewsApiService):
+class SearchPagingSource(private val apiService: NewsApiService, private val query: String):
     PagingSource<Int, Article>() {
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
         val position = params.key ?: PAGE_INDEX
-        return  try {
-            val response = apiService.trendingNews(pageNum = params.loadSize).articles
+        return try {
+            val response = apiService
+                .searchNews(query = query, pageNum = position).articles
             LoadResult.Page(
                 data = response,
-                prevKey = if (position == PAGE_INDEX) null else position -1,
+                prevKey = if (position == PAGE_INDEX) null else position - 1,
                 nextKey = if (response.isEmpty()) null else position + 1
             )
         }
-        catch (exception: IOException){
-            LoadResult.Error(exception)
+        catch (e: IOException){
+            LoadResult.Error(e)
         }
-        catch (exception: HttpException){
-            LoadResult.Error(exception)
+        catch (e: HttpException){
+            LoadResult.Error(e)
         }
     }
 }
